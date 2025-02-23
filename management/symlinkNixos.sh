3422ccb563619bf3e486e86f2d9e69ba08fefcfa
xaviduds <xaviduds@gmail.com>
2025-02-23 08:57:40 -0300

    revamp

--- management/symlinkNixos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf /home/eduardo/.config/nushell/env.nu
-rm -rf /home/eduardo/.config/nushell/config.nu
-cp -r /home/eduardo/xaviduds/dotfiles/terminal/nushell/* /home/eduardo/.config/nushell
